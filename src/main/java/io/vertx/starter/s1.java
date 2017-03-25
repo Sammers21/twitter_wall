caef01bb8eea863fef68c7119c3a21462143bd9e
Sammers21 <titantins@ya.ru>
2017-03-26 01:48:53 +0300

    ready web + messages

--- src/main/java/io/vertx/starter/s1.java
+++ src/main/java/io/vertx/starter/s1.java
@@ -3,13 +3,21 @@ package io.vertx.starter;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.eventbus.EventBus;
+import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.ext.web.handler.sockjs.BridgeOptions;
+import io.vertx.ext.web.handler.sockjs.PermittedOptions;
+import io.vertx.ext.web.handler.sockjs.SockJSHandler;
 import twitter4j.*;
 import twitter4j.conf.ConfigurationBuilder;
 
+import java.text.DateFormat;
+import java.time.Instant;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
+import io.vertx.ext.web.Router;
 
 public class s1 extends AbstractVerticle {
 
@@ -21,28 +29,36 @@ public class s1 extends AbstractVerticle {
   public void start() throws Exception {
 
     EventBus eb = vertx.eventBus();
-    DeploymentOptions options = new DeploymentOptions().setWorker(true);
 
-    vertx.deployVerticle(r1.class.getCanonicalName(), options, res -> {
-      if (res.succeeded()) {
-        System.out.println("receiver deployed successful");
-      } else if (res.failed()) {
-        System.out.println("receiver deployment failed");
-      }
-    });
+    Router router = Router.router(vertx);
+    BridgeOptions opts = new BridgeOptions()
+      .addOutboundPermitted(new PermittedOptions().setAddress("webpage"));
 
-    configTwitterSource();
+    SockJSHandler ebHandler = SockJSHandler.create(vertx).bridge(opts);
+    router.route("/eventbus/*").handler(ebHandler);
+
+    // Create a router endpoint for the static content.
+    router.route().handler(StaticHandler.create());
 
 
-    vertx.setPeriodic(5000, v -> {
+    // Start the web server and tell it to use the router to handle requests.
+    vertx.createHttpServer().requestHandler(router::accept).listen(8080);
+
+    configTwitterSource("#Dota2");
+
+
+    vertx.setPeriodic(1000, v -> {
       String tweet = getTweet();
       eb.publish("twitter dota", tweet);
+      eb.publish("webpage", tweet);
       System.out.println("sended: " + tweet);
 
     });
+
   }
 
-  private void configTwitterSource() {
+
+  private void configTwitterSource(String queryToSearch) {
     ConfigurationBuilder cb = new ConfigurationBuilder();
     cb.setDebugEnabled(true)
       .setOAuthConsumerKey("DX6ptkrAXL8iZv92IBNurhmm9")
@@ -55,14 +71,14 @@ public class s1 extends AbstractVerticle {
       Twitter twitter = new TwitterFactory(cb.build()).getInstance();
       try {
         while (true) {
-          Query query = new Query("#Dota2");
+          Query query = new Query(queryToSearch);
           QueryResult result;
           do {
             result = twitter.search(query);
             List<Status> tweets = result.getTweets();
             for (Status tweet : tweets) {
               //  System.out.println("@" + tweet.getUser().getScreenName() + " - " + tweet.getText());
-              queue.put("@" + tweet.getUser().getScreenName() + " - " + tweet.getText());
+              queue.put("@" + tweet.getUser().getScreenName() + " " + tweet.getText());
             }
           } while ((query = result.nextQuery()) != null);
         }
