5ac6c4d7204999c47b80d70bd168d29dbb4f4ec0
Sammers21 <titantins@ya.ru>
2017-04-02 16:38:24 +0300

    Non-blocking semaphore acquire + 15 min Window controll

--- src/main/java/io/vertx/starter/Server.java
+++ src/main/java/io/vertx/starter/Server.java
@@ -15,14 +15,15 @@ import io.vertx.ext.web.handler.sockjs.BridgeOptions;
 import io.vertx.ext.web.handler.sockjs.PermittedOptions;
 import io.vertx.ext.web.handler.sockjs.SockJSHandler;
 
-
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.Base64;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class Server extends AbstractVerticle {
 
@@ -32,7 +33,7 @@ public class Server extends AbstractVerticle {
     private String ConsumerSecret = "J1o1oTK5muKoDLYOw26Awcd0krZhjGaVFaC0ioKIpSoaoxyI2L";
 
     //tweet storage
-    private final BlockingQueue<String> queue = new LinkedBlockingQueue<>(pSize);
+    private final Queue<String> queue = new ConcurrentLinkedQueue<>();
 
     //for one by one requests
     private Semaphore semaphore = new Semaphore(1);
@@ -43,6 +44,10 @@ public class Server extends AbstractVerticle {
     private String query = "#love";
 
     private AtomicInteger delay = new AtomicInteger(5000);
+
+    private AtomicInteger Request15minReained = new AtomicInteger(480);
+
+    private AtomicLong last15minWindowTime = new AtomicLong(0);
 
     @Override
     public void start() throws Exception {
@@ -86,14 +91,14 @@ public class Server extends AbstractVerticle {
 
         eb.consumer("server").handler(message -> {
             // Send the message back out to all clients with the timestamp prepended.
-            System.out.println("message is: "+message.body());
+            System.out.println("message is: " + message.body());
 
             String[] split = message.body().toString().split(" ");
 
             //determine message type
             if (split[0].equals("query")) {
                 query = split[1];
-                changeQuery();
+                queue.clear();
             }
 
             if (split[0].equals("delay")) {
@@ -110,21 +115,16 @@ public class Server extends AbstractVerticle {
 
     }
 
-    private void changeQuery() {
-        try {
-            semaphore.acquire();
-            //clear old queries
-            queue.clear();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            semaphore.release();
-        }
-    }
+
 
 
     private void configTwitterSource() throws InterruptedException {
 
+        //15 min window
+        vertx.setPeriodic(60 * 15 * 1000, s -> {
+            Request15minReained.set(480);
+            last15minWindowTime.set(System.currentTimeMillis());
+        });
 
         WebClient wclient = WebClient.create(vertx,
                 new WebClientOptions()
@@ -136,36 +136,47 @@ public class Server extends AbstractVerticle {
         String BearerTokenCredentials = ConsumerKey + ":" + ConsumerSecret;
         String base64ebtc = base64encode(BearerTokenCredentials);
 
-        semaphore.acquire();
-        Buffer buffer = Buffer.buffer("grant_type=client_credentials");
-        wclient
-                .post(443, "api.twitter.com", "/oauth2/token")
-                .putHeader("Authorization", "Basic " + base64ebtc)
-                .putHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8.")
-                .sendBuffer(buffer, (AsyncResult<HttpResponse<Buffer>> ar) -> {
-                    if (ar.succeeded()) {
-                        HttpResponse<Buffer> response = ar.result();
-                        System.out.println("Got HTTP response with status " + response.statusCode());
-                        System.out.println(response.bodyAsString());
-                        System.out.println(response.bodyAsJsonObject().getString("access_token"));
-                        btoken = response.bodyAsJsonObject().getString("access_token");
-                        semaphore.release();
-                    } else {
-                        ar.cause().printStackTrace();
-                    }
-                });
+        oneMoreReqest();
+
+        authtoken(wclient, base64ebtc);
 
 
         //start queue filling
         Thread thread = new Thread(() -> {
             try {
                 while (true) {
-                    if (queue.size() > 6) {
-                        Thread.sleep(delay.get());
+                    if (btoken == null || btoken.equals("")) {
+                        System.out.println("btoken");
+                        authtoken(wclient, base64ebtc);
+                        Thread.sleep(1000);
                         continue;
                     }
-                    semaphore.acquire();
+                    if (queue.size() > 6) {
+                        System.out.println("size 6 sleep reason");
+                        System.out.println("req remained" + Request15minReained.get());
+                        Thread.sleep(1000);
+                        continue;
+                    }
+
+                    if (!isRequestReamined()) {
+                        System.out.println("no reqeus remained");
+                        vertx.eventBus().publish("webpage", "error wait please " +
+                                (System.currentTimeMillis() - last15minWindowTime.get()));
+                        Thread.sleep(System.currentTimeMillis() - last15minWindowTime.get() + 1000);
+                        continue;
+                    }
+
+
+                    if(!semaphore.tryAcquire()){
+                        continue;
+                    }
+
+                    //query to search
+                    System.out.println("req");
                     String eq = encodedQuery();
+
+                    //decrement request counter
+                    oneMoreReqest();
                     wclient
                             .get(443, "api.twitter.com", "/1.1/search/tweets.json" + eq)
                             .putHeader("Authorization", "Bearer " + btoken)
@@ -189,6 +200,25 @@ public class Server extends AbstractVerticle {
         thread.start();
     }
 
+    private void authtoken(WebClient wclient, String base64ebtc) {
+        Buffer buffer = Buffer.buffer("grant_type=client_credentials");
+        wclient
+                .post(443, "api.twitter.com", "/oauth2/token")
+                .putHeader("Authorization", "Basic " + base64ebtc)
+                .putHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8.")
+                .sendBuffer(buffer, (AsyncResult<HttpResponse<Buffer>> ar) -> {
+                    if (ar.succeeded()) {
+                        HttpResponse<Buffer> response = ar.result();
+                        System.out.println("Got HTTP response with status " + response.statusCode());
+                        System.out.println(response.bodyAsString());
+                        System.out.println(response.bodyAsJsonObject().getString("access_token"));
+                        btoken = response.bodyAsJsonObject().getString("access_token");
+                    } else {
+                        ar.cause().printStackTrace();
+                    }
+                });
+    }
+
     private String encodedQuery() {
         String q = null;
         try {
@@ -204,22 +234,27 @@ public class Server extends AbstractVerticle {
         JsonArray statuses = entries.getJsonArray("statuses");
 
         int size = statuses.size();
+        System.out.println("publish " + size + " messages");
         for (int i = 0; i < size; i++) {
 
             String name = "@" + statuses.getJsonObject(i).getJsonObject("user").getString("screen_name");
             String text = statuses.getJsonObject(i).getString("text");
 
-            try {
-                queue.put(name + " " + text);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            queue.add(name + " " + text);
 
         }
     }
 
     private String getTweet() {
         return queue.poll();
+    }
+
+    boolean isRequestReamined() {
+        return Request15minReained.get() != 0;
+    }
+
+    void oneMoreReqest() {
+        Request15minReained.decrementAndGet();
     }
 
     private String base64encode(String toencodeString) {
